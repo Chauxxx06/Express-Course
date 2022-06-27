4244f36ecc9190d6693ccda78b48b025a41eda83
Chauxxx06 <freemanchx@hotmail.com>
2022-06-27 13:38:31 +0000

    finish

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express'); // Importar en node el framework express
 const routerApi = require('./route');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/errorHandler');
-/* const { faker } = require('@faker-js/faker'); */
+const cors = require('cors'); //CORS permite que varios dominios autorizados puedan conectarse al API
 const app = express(); // se instacion el obejto de express
 const port = 3000; // se indica el puerto tcp donde escuhara el app
-
+const whiteList= ["http://localhost:8080"];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true);
+    }else{
+      callback(new Error('No Permit'));
+    }
+  }
+};
 
 /*
 //peticion GET hjacia el servidor, este deberia responde el texto anexo al send
@@ -18,6 +27,7 @@ app.get('/newEndpoint', (req, res) => {
 }); */
 
 app.use(express.json());
+app.use(cors(options));
 routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
